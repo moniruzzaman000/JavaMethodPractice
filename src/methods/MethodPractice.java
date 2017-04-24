4949ae6e97508bbd567513bbb91c0305ee48da3a
Moniruzzaman Monir <moniruzzaman000@gmail.com>
2017-04-23 20:18:30 -0400

    Code clean up

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -3,14 +3,14 @@ package methods;
 public class MethodPractice {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Exercise: 01 " + getName("Shakil Khan"));
 
 	}
-	
+
 	/**
-	 * Exercise: 01
-	 * Write a method that take your name input and return your name.
+	 * Exercise: 01 Write a method that take your name input and return your
+	 * name.
 	 * 
 	 * Parameter: String text
 	 * 
@@ -18,13 +18,13 @@ public class MethodPractice {
 	 * 
 	 * Serving Bucket: Variable
 	 */
-	
-	public static String getName(String text){
-		
+
+	public static String getName(String text) {
+
 		String name = null;
-		
+
 		name = text;
-		
+
 		return name;
 	}
 }
