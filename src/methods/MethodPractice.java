022eecc818d70cf5ae3c1ae895eb4e0704060269
Moniruzzaman Monir <moniruzzaman000@gmail.com>
2017-04-23 20:14:19 -0400

    add method exercise: 01

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -3,6 +3,28 @@ package methods;
 public class MethodPractice {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Exercise: 01 " + getName("Shakil Khan"));
 
+	}
+	
+	/**
+	 * Exercise: 01
+	 * Write a method that take your name input and return your name.
+	 * 
+	 * Parameter: String text
+	 * 
+	 * Return type: String
+	 * 
+	 * Serving Bucket: Variable
+	 */
+	
+	public static String getName(String text){
+		
+		String name = null;
+		
+		name = text;
+		
+		return name;
 	}
 }
