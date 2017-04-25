0f4373d4fc7484bb2d6ce647e3d7334d35c99b57
Moniruzzaman Monir <moniruzzaman000@gmail.com>
2017-04-25 06:10:16 -0400

    Add method Exercise: 03

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -10,10 +10,41 @@ public class MethodPractice {
 
 		System.out.println("Exercise: 02 " + getEvenNumberList(20));
 
+		System.out.println("Exercise: 03 " + getOddNumberList(20));
+
+	}
+
+	/**
+	 * Exercise: 03 Write a method that take a number input and return the List
+	 * 
+	 * of all odd number from 0 to that given number.
+	 * 
+	 * Parameter: int number
+	 * 
+	 * Return type: ArrayList<Integer>
+	 * 
+	 * Serving Bucket: List
+	 */
+
+	public static ArrayList<Integer> getOddNumberList(int number) {
+
+		ArrayList<Integer> OddNumberList = new ArrayList<Integer>();
+
+		for (int i = 0; i <= number; i++) {
+
+			if (i % 2 == 1) {
+
+				OddNumberList.add(i);
+
+			}
+		}
+
+		return OddNumberList;
 	}
 
 	/**
 	 * Exercise: 02 Write a method that take a number input and return the List
+	 * 
 	 * of all even number from 0 to that given number.
 	 * 
 	 * Parameter: int number
@@ -41,6 +72,7 @@ public class MethodPractice {
 
 	/**
 	 * Exercise: 01 Write a method that take your name input and return your
+	 * 
 	 * name.
 	 * 
 	 * Parameter: String text
